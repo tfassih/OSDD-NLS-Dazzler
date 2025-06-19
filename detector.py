d8fd08d5d46c68e7b09ab8ac18a1882b0d031dbf
tfassih <42509539+tfassih@users.noreply.github.com>
2025-06-19 15:07:09 -0500

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,10 +1,7 @@
-from math import exp
 import cv2
 import random
 import numpy as np
 import serial
-import serial.tools.list_ports
-from typing import Optional
 
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -12,13 +9,10 @@ eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml
 serial_connection_g: serial.Serial = None
 port_var = 'COM6'
 
-
-
-
 def send_servo_command(x, y):
     serial_connection = serial_connection_g
     # formula for the multiplier is field_of_view_in_degrees/(resolution_x^2 + resolution_y^2); the default assumes a 1080p resolution with a 45 degree fov. We then calculate the distance from the center of the image to determine the angle.
-    angle = (int(.0153994 * x) + int(.0153994 * y)) 
+    angle = (int(.0153994 * x) + int(.0153994 * y))
     if y < 0:
         command = f"A{angle}"
     else:
@@ -31,7 +25,8 @@ def send_servo_command(x, y):
         print(f"Arduino response: {response}")
     except serial.SerialException as e:
         serial_connection = None
-        
+
+
 def main():
     serial_connection = serial_connection_g
     if serial_connection is None:
@@ -43,8 +38,7 @@ def main():
             print("ERROR")
         else:
             serial_connection.close()
-            serial_connection = None    
-        
+            serial_connection = None
     while True:
         ret, frame = cap.read()
 
@@ -57,8 +51,9 @@ def main():
             eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
             for (ex, ey, ew, eh) in eyes:
                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
-                send_servo_command(ex, ey)
-
+                if eyes.all():
+                    print("Eyes detected")
+                    send_servo_command(ex, ey)
         cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) == ord('q'):
